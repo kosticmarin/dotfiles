23ea5c6520d7d41643705aa45d70b3c0823fcd75
Marin Koštić <kosticmarin@gmail.com>
2021-04-22 19:38:06 +0200

    Base Emacs conf

---
+++ init.el
@@ -0,0 +1,40 @@
+;;; Emacs config
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+
+(set-face-attribute 'default nil :height 160)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(load-theme 'wombat)
+
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package diminish)
+
+(use-package ivy
+  :diminish
+  :config
+  (ivy-mode 1))
+
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
