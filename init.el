fb01c1af1eb98abb3d0e48939d0b8e93e1a7277a
Marin Koštić <kosticmarin@gmail.com>
2021-04-29 22:08:27 +0200

    Add projectile to Emacs conf

--- init.el
+++ init.el
@@ -49,3 +49,11 @@
   :config
   (ivy-mode 1))
 
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
+
